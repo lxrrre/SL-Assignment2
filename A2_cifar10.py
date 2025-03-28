127770ae9164833f182e00528df63c33d7d02e49
Lorenzo <lxrre@icloud.com>
2025-03-28 19:36:54 +0100

    quasi finito 2 (saga, lorenzo)

--- A2_cifar10.py
+++ A2_cifar10.py
@@ -1,8 +1,9 @@
 # %%
 
 import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
+
+
+
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 
@@ -40,6 +41,11 @@ y_train = np.concatenate((dict1["labels"],dict2["labels"],dict3["labels"],dict4[
 X_test = test["data"]
 y_test = test["labels"]
 
+print("X_train dimensions: {}".format(X_train.shape))
+print("y_train dimensions: {}".format(y_train.shape))
+print("X_test dimensions: {}".format(X_test.shape))
+print("y_test dimensions: {}".format(np.array(y_test).shape))
+
 def data_to_image(x):
     return(x.reshape(3,32,32).transpose(1,2,0))
 
@@ -69,7 +75,7 @@ X_test= scaler.transform(X_test)
 
 
 # %%
-model = LogisticRegression(max_iter=2000, C=1e-2, verbose=1, solver='saga', multi_class='multinomial') #in scikit-learn the regularization parameter is set as the inverse of the lambda we use normally;
+model = LogisticRegression(max_iter=2000, C=1e-3, verbose=1, solver='saga') #in scikit-learn the regularization parameter is set as the inverse of the lambda we use normally;
 #saga is the minimization algorithm more adapt for large datasets, such as this one (a lot features and samples)
 model.fit(X_train, y_train)
 
@@ -83,7 +89,7 @@ print("Number of iterations: {}".format(model.n_iter_))#it's useful to check the
 
 
 # %%
-cv_model = LogisticRegressionCV(max_iter=2000, verbose=1, Cs = np.linspace(1e-4, 1e-3, 10), cv=4, n_jobs=-1, solver='saga', multi_class='multinomial') #considering high values of C bring the optimization to not converge --> strong regularization required
+cv_model = LogisticRegressionCV(max_iter=2000, verbose=1, Cs = np.linspace(1e-4, 1e-3, 10), cv=4, n_jobs=-1, solver='saga') #considering high values of C bring the optimization to not converge --> strong regularization required
 cv_model.fit(X_train, y_train)
 y_pred_cv = cv_model.predict(X_test)
 
@@ -114,6 +120,10 @@ mean_scores = np.mean(cv_model.scores_[np.unique(y_train)[0]], axis=0) # Average
 #since the score of the model doesn't depend on the class for multiclass='multinomial', we just take the first)
 C_values = cv_model.Cs_
 
+
+
+
+
 plt.figure(figsize=(8, 5))
 plt.plot(C_values, mean_scores, marker='o')
 plt.xscale('log')
@@ -125,10 +135,66 @@ plt.grid(True)
 plt.tight_layout()
 plt.show()
 
+# %%
+
+cv_model_2 = LogisticRegressionCV(max_iter=2000, verbose=1, Cs = np.linspace(1e-4, 1e-3, 10), cv=4, n_jobs=-1, solver='saga', scoring='neg_log_loss')
+cv_model_2.fit(X_train, y_train)
+y_pred_cv_2 = cv_model_2.predict(X_test)
+
+print("CV accuracy: {}".format(sum(y_pred_cv_2==y_test)/len(y_test)))
 
 
+print("Number of iterations, cv: {}".format(cv_model_2.n_iter_))
+
+print("Best value of C established: {}".format(cv_model_2.C_[0]))
 
 
+# %%
+mean_scores = np.mean(-cv_model_2.scores_[np.unique(y_train)[0]], axis=0) # Average accuracy on the 4 folds for each C value #np.unique(y_train) takes all diverse values of y_train and generates an np-array
+#since the score of the model doesn't depend on the class
+C_values = cv_model_2.Cs_
+
+print(mean_scores)
+
+plt.figure(figsize=(8, 5))
+plt.plot(C_values, mean_scores, marker='o')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Regularization parameter C (log scale)')
+plt.ylabel('Cross-validated accuracy (log scale)')
+plt.title('Log-log plot of CV logarithmic scoring rule vs. C')
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+#%%
+
+# %% Use the entire dataset with C=7e-4
+
+X_train_full = np.concatenate((dict1["data"], dict2["data"], dict3["data"], dict4["data"], dict5["data"]))
+y_train_full = np.concatenate((dict1["labels"], dict2["labels"], dict3["labels"], dict4["labels"], dict5["labels"]))
+X_test_full = test["data"]
+y_test_full = test["labels"]
 
 
+scaler = StandardScaler()
+X_train_full = scaler.fit_transform(X_train_full)
+X_test_full = scaler.transform(X_test_full)
 
+# Train logistic regression on full data with best C
+final_model = LogisticRegression(max_iter=2000, C=7e-4, solver='saga', verbose=1)
+final_model.fit(X_train_full, y_train_full)
+
+#
+y_pred_full = final_model.predict(X_test_full)
+accuracy_full = np.mean(np.array(y_pred_full) == np.array(y_test_full))
+
+print("Final model test accuracy on full dataset: {:.3f}".format(accuracy_full))
+print("Number of iterations: {}".format(final_model.n_iter_))
+
+
+#%%
+from sklearn.metrics import accuracy_score, log_loss
+
+
+train_accuracy = accuracy_score(y_train
