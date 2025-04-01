b260e4d5843815b60817828b64cb711e1684ff4e
Lorenzo <lxrre@icloud.com>
2025-04-01 12:50:26 +0200

    ritoccato leggermente A2_francesco

--- A2_cifar10.py
+++ A2_cifar10.py
@@ -1,8 +1,11 @@
+import os
+print(os.getcwd())
+
 # %%
 
 import numpy as np
 
-
+import time
 
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
@@ -13,12 +16,6 @@ warnings.simplefilter("always", ConvergenceWarning) #to understand better the co
 
 from sklearn.preprocessing import StandardScaler  #not required but useful, we can think about this
 
-from sklearn.model_selection import StratifiedKFold
-
-import pandas as pd
-
-
-
 
 def unpickle(file):
     import pickle
@@ -26,13 +23,13 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='latin1')
     return dict
 
-dict1 = unpickle("./data/cifar-10-batches-py/data_batch_1")
-dict2 = unpickle("./data/cifar-10-batches-py/data_batch_2")
-dict3 = unpickle("./data/cifar-10-batches-py/data_batch_3")
-dict4 = unpickle("./data/cifar-10-batches-py/data_batch_4")
-dict5 = unpickle("./data/cifar-10-batches-py/data_batch_5")
-test = unpickle("./data/cifar-10-batches-py/test_batch")
-meta_data = unpickle("./data/cifar-10-batches-py/batches.meta")
+dict1 = unpickle("./SL-Assignment2/data/cifar-10-batches-py/data_batch_1")
+dict2 = unpickle("./SL-Assignment2/data/cifar-10-batches-py/data_batch_2")
+dict3 = unpickle("./SL-Assignment2/data/cifar-10-batches-py/data_batch_3")
+dict4 = unpickle("./SL-Assignment2/data/cifar-10-batches-py/data_batch_4")
+dict5 = unpickle("./SL-Assignment2/data/cifar-10-batches-py/data_batch_5")
+test = unpickle("./SL-Assignment2/data/cifar-10-batches-py/test_batch")
+meta_data = unpickle("./SL-Assignment2/data/cifar-10-batches-py/batches.meta")
 label_names = meta_data["label_names"]
 
 
@@ -55,119 +52,135 @@ def plot_image(image, title=""):
     fig.axes.set_axis_off()
     plt.show()
 
+def plot_scores(C_values, scores, loss_type, img_name):
+    plt.figure(figsize=(8, 5))
+    plt.plot(C_values, mean_scores, marker='o')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel('Regularization parameter C (log scale)')
+    plt.ylabel('Cross-validated '+loss_type+' accuracy (log scale)')
+    plt.title('Log-log plot of CV '+loss_type+' vs. C')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(img_name)
+
+
 # as a verification that everything is working correctly, plot an image
 #image_nr = 320
 #plot_image(X_train[image_nr,:],label_names[y_train[image_nr]])
 
-#the next 4 lines are to work with a smaller dataset since my (Lorenzo) pc is not so quick with these computations
+# %%
+#the next 4 lines are to work with a smaller dataset if we want to avoid computational overhead
 X_train = X_train[:8000]
 y_train = y_train[:8000]
 X_test = X_test[:2000]
 y_test = y_test[:2000]
 
+# %%
 
-#da inserire o meno, si può valutare --> ho notato che mettendola migliora il problema delle iterazioni nulle nella cross-validation:
-#potrebbe esulare da quanto richiesto nell'assignment
+#scaling is very useful to improve the overall performances
 
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test= scaler.transform(X_test)
 
+time_start = time.time()
+model_saga = LogisticRegression(max_iter=5000, C=1e-3, verbose=1, solver='saga') #in scikit-learn the regularization parameter is set as the inverse of the lambda we use normally;
+#saga is the minimization algorithm (which should be) more adapt for large datasets, such as this one (a lot features and samples)
+model_saga.fit(X_train, y_train)
+time_end = time.time()
+time_saga = time_end - time_start
+
+print("saga time elapsed: {}".format(time_saga))
+
+time_start = time.time()
+model_lbgfs = LogisticRegression(max_iter=5000, C=1e-3, verbose=1) #here we use lbgfs instead of saga
+model_lbgfs.fit(X_train, y_train)
+time_end = time.time()
+time_LBGFS = time_end - time_start
+
+print("lbgfs time elapsed: {}".format(time_LBGFS))
+
+
+print("lbgfs accuracy: {}".format(model_lbgfs.score(X_test, y_test)))
+print("saga accuracy: {}".format(model_saga.score(X_test, y_test)))
+
+print("Number of iterations, lbgfs: {}".format(model_lbgfs.n_iter_))
+print("Number of iterations, saga: {}".format(model_saga.n_iter_))#it's useful to check the number of iterations
+
 
 # %%
-model = LogisticRegression(max_iter=2000, C=1e-3, verbose=1, solver='saga') #in scikit-learn the regularization parameter is set as the inverse of the lambda we use normally;
-#saga is the minimization algorithm more adapt for large datasets, such as this one (a lot features and samples)
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
-
-#plot_image(X_test_small[250,:], label_names[y_pred[250]])
-
-print("accuracy: {}".format(sum(y_pred==y_test)/len(y_test))) #quick assessment of the quality of the predictions
-
-print("Number of iterations: {}".format(model.n_iter_))#it's useful to check the number of iterations
-
-
-# %%
-cv_model = LogisticRegressionCV(max_iter=2000, verbose=1, Cs = np.linspace(1e-4, 1e-3, 10), cv=4, n_jobs=-1, solver='saga') #considering high values of C bring the optimization to not converge --> strong regularization required
+#we discard SAGA as it's considerably slower; first we use a larger range of Cs
+cv_model = LogisticRegressionCV(max_iter=5000, verbose=1, Cs = np.linspace(1e-7, 1e-2, 10), cv=4, n_jobs=-1) #considering high values of C bring the optimization to not converge --> strong regularization required
 cv_model.fit(X_train, y_train)
-y_pred_cv = cv_model.predict(X_test)
 
-print("CV accuracy: {}".format(sum(y_pred_cv==y_test)/len(y_test)))
+
+
+print("CV accuracy: {}".format(cv_model.score(X_test, y_test)))
 
 
 print("Number of iterations, cv: {}".format(cv_model.n_iter_))
-#n_iter è di dimensione n_fold x n_Cs
-#anche con lo scaling continuo ad avere 0 (che non hanno senso) per valori di C grandi (ossia regolarizzazione limitata)
-#per giunta questo problema compare solo con la cv: per lo stesso valore di C la regressione logistica normale va (ha un certo numero di iterazioni)
+#n_iter is of dimensions n_fold x n_Cs
+
 
 print("Best value of C established: {}".format(cv_model.C_[0]))
 
-"""
-cv = StratifiedKFold(n_splits=4)
-
-for i, (_, val_idx) in enumerate(cv.split(X_train, y_train)):
-    print(f"Fold {i}: classi presenti = {np.unique(y_train[val_idx])}")
-
-#questo for serve per assicurarsi che n_iter=0 NON dipenda dai fold, ma soltanto da alcuni valori di C
-
-"""
-
-
-# %%
-
-mean_scores = np.mean(cv_model.scores_[np.unique(y_train)[0]], axis=0) # Average accuracy on the 4 folds for each C value #np.unique(y_train) takes all diverse values of y_train and generates an np-array
+mean_scores = np.mean(cv_model.scores_[np.unique(y_train)[0]], axis=0) # Average accuracy on the 4 folds for each C value; #np.unique(y_train) takes all diverse values of y_train and generates an np-array
 #since the score of the model doesn't depend on the class for multiclass='multinomial', we just take the first)
 C_values = cv_model.Cs_
 
+plot_scores(C_values, mean_scores, loss_type='accuracy', img_name='accuracy_C_large')
 
-
-
-
-plt.figure(figsize=(8, 5))
-plt.plot(C_values, mean_scores, marker='o')
-plt.xscale('log')
-plt.yscale('log')
-plt.xlabel('Regularization parameter C (log scale)')
-plt.ylabel('Cross-validated accuracy (log scale)')
-plt.title('Log-log plot of CV accuracy vs. C')
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+print("\nAverage accuracy over the folds for each C:")
+for C, score in zip(C_values, mean_scores):
+    print(f"C = {C:.2e} | Accuracy: {score:.4f}")
 
 # %%
+#here we refine the search for the best C by considering a shorter range of values
 
-cv_model_2 = LogisticRegressionCV(max_iter=2000, verbose=1, Cs = np.linspace(1e-4, 1e-3, 10), cv=4, n_jobs=-1, solver='saga', scoring='neg_log_loss')
+cv_model_2 = LogisticRegressionCV(max_iter=5000, verbose=1, Cs = np.linspace(1e-4, 1e-3, 10), cv=4, n_jobs=-1)
 cv_model_2.fit(X_train, y_train)
 y_pred_cv_2 = cv_model_2.predict(X_test)
 
-print("CV accuracy: {}".format(sum(y_pred_cv_2==y_test)/len(y_test)))
-
+print("CV accuracy: {}".format(cv_model_2.score(X_test, y_test)))
 
 print("Number of iterations, cv: {}".format(cv_model_2.n_iter_))
 
 print("Best value of C established: {}".format(cv_model_2.C_[0]))
 
+mean_scores = np.mean(cv_model_2.scores_[np.unique(y_train)[0]], axis=0)
 
-# %%
-mean_scores = np.mean(-cv_model_2.scores_[np.unique(y_train)[0]], axis=0) # Average accuracy on the 4 folds for each C value #np.unique(y_train) takes all diverse values of y_train and generates an np-array
-#since the score of the model doesn't depend on the class
 C_values = cv_model_2.Cs_
 
-print(mean_scores)
+plot_scores(C_values, mean_scores, loss_type='accuracy', img_name='accuracy_C_short')
 
-plt.figure(figsize=(8, 5))
-plt.plot(C_values, mean_scores, marker='o')
-plt.xscale('log')
-plt.yscale('log')
-plt.xlabel('Regularization parameter C (log scale)')
-plt.ylabel('Cross-validated accuracy (log scale)')
-plt.title('Log-log plot of CV logarithmic scoring rule vs. C')
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+print("\nAverage accuracy over the folds for each C:")
+for C, score in zip(C_values, mean_scores):
+    print(f"C = {C:.2e} | Accuracy: {score:.4f}")
 
-#%%
+
+
+# %%
+#here we consider the neg-log-likelihood
+cv_model_nll = LogisticRegressionCV(max_iter=5000, verbose=1, Cs = np.linspace(1e-4, 1e-3, 10), cv=4, n_jobs=-1, scoring='neg_log_loss')
+cv_model_nll.fit(X_train, y_train)
+
+print("CV accuracy: {}".format(cv_model_nll.score(X_test, y_test)))
+
+print("Number of iterations, cv: {}".format(cv_model_nll.n_iter_))
+
+print("Best value of C established: {}".format(cv_model_nll.C_[0]))
+
+mean_scores = np.mean(cv_model_nll.scores_[np.unique(y_train)[0]], axis=0)
+
+C_values = cv_model_nll.Cs_
+
+plot_scores(C_values, mean_scores, loss_type='logarithmic score', img_name='nll_C')
+
+print("\nAverage accuracy over the folds for each C:")
+for C, score in zip(C_values, mean_scores):
+    print(f"C = {C:.2e} | Accuracy.: {score:.4f}")
+
 
 # %% Use the entire dataset with C=7e-4
 
@@ -200,7 +213,7 @@ from sklearn.metrics import accuracy_score, log_loss
 train_accuracy = accuracy_score(y_train, final_model.predict(X_train))
 test_accuracy = accuracy_score(y_test, final_model.predict(X_test))
 
-ì
+
 train_log_loss = log_loss(y_train, final_model.predict_proba(X_train))
 test_log_loss = log_loss(y_test, final_model.predict_proba(X_test))
 
--- A2_cifar10_francesco.py
+++ A2_cifar10_francesco.py
@@ -11,9 +11,9 @@ from sklearn.exceptions import ConvergenceWarning
 warnings.simplefilter("always", ConvergenceWarning)
 
 class A2_cifar10:
-    def __init__(self):
-        self.solver = 'lbfgs'
-        self.verbose = 0
+    def __init__(self, solver_tag = 'lbfgs'):
+        self.solver = solver_tag
+        self.verbose = 1 # makes it so that more comments are printed while training
         self.scaler = StandardScaler()
         self.folds= 4
         self.subset_frac = 0.2  # Portion of the full dataset to use
@@ -48,7 +48,7 @@ class A2_cifar10:
             Cs=self.C_values,
             cv=self.folds,
             solver=self.solver,
-            n_jobs=-1,
+            n_jobs=-1, #parallel computing for efficiency
             verbose=self.verbose,
             scoring=scoring
         )
@@ -56,13 +56,13 @@ class A2_cifar10:
         
         # Plot results
         mean_scores = np.mean(cv_model.scores_[0], axis=0)
-        plt.figure(figsize=(8, 5))
+        plt.figure(figsize=(10, 7))
         plt.loglog(cv_model.Cs_, mean_scores, marker='o', linestyle='-')  
         plt.xticks(cv_model.Cs_, labels=[f"{c:.2e}" for c in cv_model.Cs_], rotation=90)
         plt.xlabel('Regularization parameter C')
-        plt.ylabel('Score' if scoring == 'accuracy' else 'Log Loss')
+        plt.ylabel('Accuracy' if scoring == 'accuracy' else 'Log Loss')
         plt.grid(True, which='both')
-        plt.show()
+        plt.savefig('Cs_vs_scores.png')
         return cv_model
 
     def manual_log_loss_cv(self, X_train_scaled, y_train_small):
@@ -88,13 +88,13 @@ class A2_cifar10:
                 
             log_losses.append(np.mean(fold_scores))
         
-        plt.figure(figsize=(8, 5))
+        plt.figure(figsize=(10, 7))
         plt.loglog(self.C_values, log_losses, marker='o', linestyle='-')
         plt.xticks(self.C_values, labels=[f"{c:.2e}" for c in self.C_values], rotation=90)
         plt.xlabel('Regularization parameter C')
         plt.ylabel('Log Loss')
         plt.grid(True, which='both')
-        plt.show()
+        plt.savefig('Cs_vs_log_loss_cv.png')
         
         best_C = self.C_values[np.argmin(log_losses)]
         return best_C
@@ -118,64 +118,75 @@ class A2_cifar10:
             print(f"{name} Log-Loss: {loss:.4f}")
 
 
-def main():
-    # Execution time
-    load_start = time.time()
+#%%
+# Execution time
+load_start = time.time()
 
-    # Load data
-    model_instance = A2_cifar10()
-    train_batches = [model_instance.unpickle(f"./cifar-10-batches-py/data_batch_{i}") for i in range(1,6)]
-    test_batch = model_instance.unpickle("./cifar-10-batches-py/test_batch")
-    meta_data = model_instance.unpickle("./cifar-10-batches-py/batches.meta")
+# Load data
+model_instance = A2_cifar10()
+model_instance_saga = A2_cifar10(solver_tag='saga')
+train_batches = [model_instance.unpickle(f"./data/cifar-10-batches-py/data_batch_{i}") for i in range(1,6)]
+test_batch = model_instance.unpickle("./data/cifar-10-batches-py/test_batch")
+meta_data = model_instance.unpickle("./data/cifar-10-batches-py/batches.meta")
 
-    # Prepare data
-    X_train = np.concatenate([batch["data"] for batch in train_batches])
-    y_train = np.concatenate([batch["labels"] for batch in train_batches])
-    X_test, y_test = test_batch["data"], test_batch["labels"]
+# Prepare data
+X_train = np.concatenate([batch["data"] for batch in train_batches])
+y_train = np.concatenate([batch["labels"] for batch in train_batches])
+X_test, y_test = test_batch["data"], test_batch["labels"]
 
-    # Create smaller subset for development
-    train_subset_size, test_subset_size = int(len(X_train) * model_instance.subset_frac), int(len(X_test) * model_instance.subset_frac)
-    X_train_small, y_train_small = X_train[:train_subset_size], y_train[:train_subset_size]
-    X_test_small, y_test_small = X_test[:test_subset_size], y_test[:test_subset_size]
+# Create smaller subset for development
+train_subset_size, test_subset_size = int(len(X_train) * model_instance.subset_frac), int(len(X_test) * model_instance.subset_frac)
+X_train_small, y_train_small = X_train[:train_subset_size], y_train[:train_subset_size]
+X_test_small, y_test_small = X_test[:test_subset_size], y_test[:test_subset_size]
 
-    # Standardize data
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train_small)
-    X_test_scaled = scaler.transform(X_test_small)
+# Standardize data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train_small)
+X_test_scaled = scaler.transform(X_test_small)
 
-    # Initial model training
-    print("Training base model:\nDataset train size: ", len(X_train_small), "\nDataset test size: ", len(X_test_small),"\n")
-    base_model = model_instance.train_and_evaluate_model(1e-3, X_train_scaled, y_train_small, X_test_scaled, y_test_small)
+#%%
+# Initial model training
+print("Training base model:\nDataset train size: ", len(X_train_small), "\nDataset test size: ", len(X_test_small),"\n")
+base_model = model_instance.train_and_evaluate_model(1e-3, X_train_scaled, y_train_small, X_test_scaled, y_test_small)
+lbgfs_time = time.time() - load_start
+print("Training lbgfs base model took ", lbgfs_time)
+saga_start_time = time.time()
+saga_model = model_instance_saga.train_and_evaluate_model(1e-3, X_train_scaled, y_train_small, X_test_scaled, y_test_small)
+saga_time = time.time() - saga_start_time
+print("Training saga base model took ", saga_time)
 
-    # Cross-validated model training
-    print("\nRunning cross-validation with accuracy scoring:")
-    cv_model_acc = model_instance.run_cross_validation(X_train_scaled, y_train_small, scoring='accuracy')
-    best_C_acc = cv_model_acc.C_[0]
-    mean_scores_acc = np.mean(cv_model_acc.scores_[0], axis=0)
-    best_C_acc_idx = np.argmin(np.abs(model_instance.C_values - best_C_acc))
-    best_acc_value = mean_scores_acc[best_C_acc_idx]
-    print(f"Best C (accuracy): {best_C_acc:.2e} with accuracy {best_acc_value:.3f}")
+ #%%
+# Cross-validated model training
+print("\nRunning cross-validation with accuracy scoring:")
+cv_model_acc = model_instance.run_cross_validation(X_train_scaled, y_train_small, scoring='accuracy')
+best_C_acc = cv_model_acc.C_[0]
+mean_scores_acc = np.mean(cv_model_acc.scores_[0], axis=0)
+best_C_acc_idx = np.argmin(np.abs(model_instance.C_values - best_C_acc))
+best_acc_value = mean_scores_acc[best_C_acc_idx]
+print(f"Best C (accuracy): {best_C_acc:.2e} with accuracy {best_acc_value:.3f}")
 
-    print("\nRunning cross-validation with log loss scoring:")
-    best_C_log_loss = model_instance.manual_log_loss_cv(X_train_scaled, y_train_small)
-    best_C_log_loss_idx = np.argmin(np.abs(model_instance.C_values - best_C_log_loss))
-    best_log_loss_accuracy = mean_scores_acc[best_C_log_loss_idx]
-    print(f"Best C (log loss): {best_C_log_loss:.2e} with accuracy {best_log_loss_accuracy:.3f}")
+#%%
+print("\nRunning cross-validation with log loss scoring:")
+best_C_log_loss = model_instance.manual_log_loss_cv(X_train_scaled, y_train_small)
+best_C_log_loss_idx = np.argmin(np.abs(model_instance.C_values - best_C_log_loss))
+best_log_loss_accuracy = mean_scores_acc[best_C_log_loss_idx]
+print(f"Best C (log loss): {best_C_log_loss:.2e} with accuracy {best_log_loss_accuracy:.3f}")
 
-    # Full dataset training
-    print("\nTraining final model on full dataset:")
-    scaler_full = StandardScaler()
-    X_train_full = scaler_full.fit_transform(X_train)
-    X_test_full = scaler_full.transform(X_test)
+#%%
+# Full dataset training
+print("\nTraining final model on full dataset:")
+scaler_full = StandardScaler()
+X_train_full = scaler_full.fit_transform(X_train)
+X_test_full = scaler_full.transform(X_test)
 
-    # Final model training
-    final_model_1 = model_instance.make_final_model(best_C_acc, X_train_full, y_train) 
+# Final model training
+final_model_1 = model_instance.make_final_model(best_C_log_loss, X_train_full, y_train)
 
-    # Evaluate final model
-    if best_C_acc == best_C_log_loss:
+# Evaluate final model
+if best_C_acc == best_C_log_loss:
             print("\nEvaluating final models based on best regularization parameter (is the same for accuracy and log loss):")
             model_instance.print_metrics(final_model_1, X_train_full, X_test_full, y_train, y_test)
-    else:   
+else:
         final_model_2 = model_instance.make_final_model(best_C_log_loss, X_train_full, y_train)
 
         print("\nEvaluating final models based on best regularization parameter for accuracy:")
@@ -183,9 +194,8 @@ def main():
         print("\nEvaluating final models based on best regularization parameter for log loss:")
         model_instance.print_metrics(final_model_2, X_train_full, X_test_full, y_train, y_test)
 
-    total_time = time.time() - load_start
-    print(f"\nTotal execution time: {total_time:.1f} seconds")
-    print("All tasks completed!")
+total_time = time.time() - load_start
+print(f"\nTotal execution time: {total_time:.1f} seconds")
+print("All tasks completed!")
 
-if __name__ == "__main__":
-    main()
+
